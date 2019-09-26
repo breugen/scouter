dc5ac1cd671b4eb2ad6224e42467ce04f6e50f2d
ebreaur <ebreaur@gmail.com>
2019-09-26 14:52:58 +0300

    Format values in the output json

--- app.js
+++ app.js
@@ -11,25 +11,45 @@ console.log('Scouter started');
 
 xlsxj({
     input: "database/paths.xlsx", 
-    output: "output/points.json",
+    output: "output/points-raw.json",
     sheet: "puncte"
-  }, (err, result) => {
+  }, (err, points) => {
     if (err) {
-        console.log('Scouter failed to discovered the points');
+      console.log(err, 'Scouter failed to discover the points');
     } else {
-        console.log('Scouter discovered the points');
+      const processedPoints = points.map(point => {
+        point.id = parseInt(point.id);
+        point.altitude = parseInt(point.id);
+        point.publicTransport = Boolean(point.publicTransport);
+        point.car = Boolean(point.car);
+        return point;
+      });
+
+      fs.writeFileSync('output/points.json', JSON.stringify(processedPoints));
+
+      console.log('Scouter discovered the points');
     }
   }
 );
 
 xlsxj({
     input: "database/paths.xlsx", 
-    output: "output/tracks.json",
+    output: "output/trails-raw.json",
     sheet: "trasee"
-  }, (err, result) => {
+  }, (err, trails) => {
     if (err) {
         console.log('Scouter failed to discovered the trails');
     } else {
+        const processedTrails = trails.map(trail => {
+          trail.id = parseInt(trail.id);
+          trail.points = JSON.parse(trail.points);
+          trail.time = parseInt(trail.time);
+          trail.reverseTime = parseInt(trail.reverseTime);
+          return trail;
+        });
+
+        fs.writeFileSync('output/trails.json', JSON.stringify(processedTrails));
+
         console.log('Scouter discovered the trails');
     }
   }
